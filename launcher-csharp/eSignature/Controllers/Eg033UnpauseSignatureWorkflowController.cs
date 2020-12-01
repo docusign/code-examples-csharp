4960d80314f8eec825e150f3f236fdabeba2d7ac
meihDS <70775251+meihDS@users.noreply.github.com>
2020-12-01 14:21:19 -0800

    Sync with public; fix typo

--- launcher-csharp/eSignature/Controllers/Eg033UnpauseSignatureWorkflowController.cs
+++ launcher-csharp/eSignature/Controllers/Eg033UnpauseSignatureWorkflowController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using DocuSign.CodeExamples.Controllers;
 using DocuSign.CodeExamples.Models;
 using DocuSign.eSign.Api;
@@ -53,8 +53,8 @@ namespace DocuSign.CodeExamples.eSignature.Controllers
             var envelope = CreateEnvelope();
 
             // Step 4. Call the eSignature API
-            var updateOptinons = new UpdateOptions() { resendEnvelope = "true" };
-            var results = envelopesApi.Update(accountId, RequestItemsService.PausedEnvelopeId, envelope, updateOptinons);
+            var updateOptions = new UpdateOptions() { resendEnvelope = "true" };
+            var results = envelopesApi.Update(accountId, RequestItemsService.PausedEnvelopeId, envelope, updateOptions);
 
             // Process results
             ViewBag.h1 = "The envelope was updated successfully!";
