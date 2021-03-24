43f425bc453666346631b18e0983c822fe985103
annahileta <anna.hileta@sigma.software>
2021-03-24 11:34:09 +0200

    changed all formating to tabs

--- launcher-csharp/Monitor/Controllers/MonitorController.cs
+++ launcher-csharp/Monitor/Controllers/MonitorController.cs
@@ -8,34 +8,34 @@ namespace DocuSign.CodeExamples.Controllers
     [Route("monitorExample001")]
     public class MonitorController : EgController
     {
-		private IRequestItemsService _requestItemsService;
-		public MonitorController(DSConfiguration config, IRequestItemsService requestItemsService)
+        private IRequestItemsService _requestItemsService;
+        public MonitorController(DSConfiguration config, IRequestItemsService requestItemsService)
             : base(config, requestItemsService)
         {
             ViewBag.title = "How to get monitoring data";
-			_requestItemsService = requestItemsService;
-		}
+            _requestItemsService = requestItemsService;
+        }
 
         public override string EgName => "monitorExample001";
 
-		[HttpPost]
-		public IActionResult Create()
-		{
-			// Obtain your JWT authentication token
-			this._requestItemsService.UpdateUserFromJWT();
+        [HttpPost]
+        public IActionResult Create()
+        {
+            // Obtain your JWT authentication token
+            this._requestItemsService.UpdateUserFromJWT();
 
-			// Preparing data for this method
-			string accessToken = RequestItemsService.User.AccessToken;
-			string requestPath = "https://lens-d.docusign.net/api/v2.0/datasets/monitor/";
+            // Preparing data for this method
+            string accessToken = RequestItemsService.User.AccessToken;
+            string requestPath = "https://lens-d.docusign.net/api/v2.0/datasets/monitor/";
 
-			// Getting monitoring data
-			var results = GetMonitoringData.DoWork(accessToken, requestPath);
+            // Getting monitoring data
+            var results = GetMonitoringData.DoWork(accessToken, requestPath);
 
-			// Process results
-			ViewBag.h1 = "Response output";
-			ViewBag.message = "Monitor data responce output:";
-			ViewBag.Locals.Json = JsonConvert.SerializeObject(results, Formatting.Indented);
-			return View("example_done");
-		}
-	}
+            // Process results
+            ViewBag.h1 = "Response output";
+            ViewBag.message = "Monitor data responce output:";
+            ViewBag.Locals.Json = JsonConvert.SerializeObject(results, Formatting.Indented);
+            return View("example_done");
+        }
+    }
 }
--- launcher-csharp/Monitor/Examples/GetMonitoringData.cs
+++ launcher-csharp/Monitor/Examples/GetMonitoringData.cs
@@ -9,60 +9,60 @@ namespace DocuSign.CodeExamples.Monitor.Examples
 {
     public class GetMonitoringData : Controller
     {
-		/// <summary>
-		/// Gets data from monitor
-		/// </summary>
-		/// <param name="requestPath">Request path, used for API calls (URI)</param>
-		/// <param name="accessToken">Access Token for API call (JWT OAuth)</param>
-		/// <returns>The list of JObjects, containing data from monitor</returns>
-		public static List<JObject> DoWork(string accessToken, string requestPath)
-		{
-			//  Construct your API headers
-			WebHeaderCollection headers = new WebHeaderCollection();
-			headers.Add("Authorization", String.Format("Bearer {0}", accessToken));
-			headers.Add("Content-Type", "application/json");
+        /// <summary>
+        /// Gets data from monitor
+        /// </summary>
+        /// <param name="requestPath">Request path, used for API calls (URI)</param>
+        /// <param name="accessToken">Access Token for API call (JWT OAuth)</param>
+        /// <returns>The list of JObjects, containing data from monitor</returns>
+        public static List<JObject> DoWork(string accessToken, string requestPath)
+        {
+            //  Construct API headers
+            WebHeaderCollection headers = new WebHeaderCollection();
+            headers.Add("Authorization", String.Format("Bearer {0}", accessToken));
+            headers.Add("Content-Type", "application/json");
 
-			// Declare variables
-			bool complete = false;
-			string cursorValue = "";
-			int limit = 2; // Amount of records you want to read in one request
-			List<JObject> functionResult = new List<JObject>();
+            // Declare variables
+            bool complete = false;
+            string cursorValue = "";
+            int limit = 2; // Amount of records you want to read in one request
+            List<JObject> functionResult = new List<JObject>();
 
-			// Get monitoring data
-			do
-			{
-				var cursorValueFormated = (!string.IsNullOrEmpty(cursorValue)) ? $"={cursorValue}" : cursorValue;
+            // Get monitoring data
+            do
+            {
+                var cursorValueFormated = (!string.IsNullOrEmpty(cursorValue)) ? $"={cursorValue}" : cursorValue;
 
-				// Add cursor value and amount of records to read to the request
-				var requestParameters = String.Format("stream?cursor{0}&limit={1}",
-					cursorValueFormated, limit);
+                // Add cursor value and amount of records to read to the request
+                var requestParameters = String.Format("stream?cursor{0}&limit={1}",
+                cursorValueFormated, limit);
 
-				WebRequest request = WebRequest.Create(requestPath + requestParameters);
-				request.Headers = headers;
+                WebRequest request = WebRequest.Create(requestPath + requestParameters);
+                request.Headers = headers;
 
-				Stream requestStream = request.GetResponse().GetResponseStream();
-				StreamReader requestStreamReader = new StreamReader(requestStream);
+                Stream requestStream = request.GetResponse().GetResponseStream();
+                StreamReader requestStreamReader = new StreamReader(requestStream);
 
-				string result = requestStreamReader.ReadToEnd();
+                string result = requestStreamReader.ReadToEnd();
 
-				// Parse result to JSON format
-				JObject resultJson = JObject.Parse(result);
-				string endCursor = resultJson.GetValue("endCursor").ToString();
+                // Parse result to JSON format
+                JObject resultJson = JObject.Parse(result);
+                string endCursor = resultJson.GetValue("endCursor").ToString();
 
-				// If the endCursor from the response is the same as the one that you already have,
-				// it means that you have reached the end of the records
-				if (endCursor == cursorValue)
-				{
-					complete = true;
-				}
-				else
-				{
-					cursorValue = endCursor;
-					functionResult.Add(resultJson);
-				}
-			} while (!complete);
+                // If the endCursor from the response is the same as the one that you already have,
+                // it means that you have reached the end of the records
+                if (endCursor == cursorValue)
+                {
+                    complete = true;
+                }
+                else
+                {
+                    cursorValue = endCursor;
+                    functionResult.Add(resultJson);
+                }
+            } while (!complete);
 
-			return functionResult;
-		}
-	}
+            return functionResult;
+        }
+    }
 }
