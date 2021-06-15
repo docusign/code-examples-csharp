357fd3deb5733446de2ce3465bcbe232df284b43
meihDS <70775251+meihDS@users.noreply.github.com>
2021-06-15 09:17:57 -0700

    Devdocs 4959 2 (#32)
    * Remove unused EmbeddedSigningModel class
    
    * Home, eSign1, eSign34
    
    * add to Homepage
    
    Co-authored-by: Jason Kamleh <jason.kamleh@nci.com.au>
    Co-authored-by: Inbar Gazit <inbar.gazit@docusign.com>

--- launcher-csharp/eSignature/Models/EmbeddedSigningModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DocuSign.CodeExamples.Views
-{
-    public class EmbeddedSigningModel
-    {
-        public string SignerEmail { get; set; }
-        public string SignerName { get; set; }
-    }
-}
