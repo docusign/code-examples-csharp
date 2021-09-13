836ddbeded5f5efce05c75e4e70b10040c6bbbd3
Inbar Gazit <inbar.gazit@docusign.com>
2021-09-13 15:07:54 -0700

    Should be good now

--- launcher-csharp/eSignature/Controllers/Eg020PhoneAuthController.cs
+++ launcher-csharp/eSignature/Controllers/Eg020PhoneAuthController.cs
@@ -1,6 +1,7 @@
 using DocuSign.CodeExamples.Models;
 using Microsoft.AspNetCore.Mvc;
 using eSignature.Examples;
+using DocuSign.eSign.Client;
 
 namespace DocuSign.CodeExamples.Controllers
 {
@@ -19,35 +20,45 @@ namespace DocuSign.CodeExamples.Controllers
         [HttpPost]
         public IActionResult Create(string signerEmail, string signerName, string signerCountryCode, string signerPhoneNumber)
         {
-            // Check the token with minimal buffer time.
-            bool tokenOk = CheckToken(3);
-            if (!tokenOk)
+            try
             {
-                // We could store the parameters of the requested operation so it could be 
-                // restarted automatically. But since it should be rare to have a token issue
-                // here, we'll make the user re-enter the form data after authentication.
-                RequestItemsService.EgName = EgName;
-                return Redirect("/ds/mustAuthenticate");
+                // Check the token with minimal buffer time.
+                bool tokenOk = CheckToken(3);
+                if (!tokenOk)
+                {
+                    // We could store the parameters of the requested operation so it could be 
+                    // restarted automatically. But since it should be rare to have a token issue
+                    // here, we'll make the user re-enter the form data after authentication.
+                    RequestItemsService.EgName = EgName;
+                    return Redirect("/ds/mustAuthenticate");
+                }
+
+                // Data for this method:
+                // signerEmail 
+                // signerName            
+                var basePath = RequestItemsService.Session.BasePath + "/restapi";
+
+                // Obtain your OAuth token
+                var accessToken = RequestItemsService.User.AccessToken; // Represents your {ACCESS_TOKEN}
+                var accountId = RequestItemsService.Session.AccountId; // Represents your {ACCOUNT_ID}
+
+                // Call the Examples API method to create an envelope and 
+                // add recipient that is to be authenticated with phone call
+                string envelopeId = RecipientAuthPhone.CreateEnvelopeWithRecipientUsingPhoneAuth(signerEmail, signerName,
+                    accessToken, basePath, accountId, signerCountryCode, signerPhoneNumber);
+
+                // Process results
+                ViewBag.h1 = "Envelope sent";
+                ViewBag.message = "The envelope has been created and sent!<br />Envelope ID " + envelopeId + ".";
+                return View("example_done");
             }
+            catch (ApiException apiException)
+            {
+                ViewBag.errorCode = apiException.ErrorCode;
+                ViewBag.errorMessage = apiException.Message;
 
-            // Data for this method:
-            // signerEmail 
-            // signerName            
-            var basePath = RequestItemsService.Session.BasePath + "/restapi";
-
-            // Obtain your OAuth token
-            var accessToken = RequestItemsService.User.AccessToken; // Represents your {ACCESS_TOKEN}
-            var accountId = RequestItemsService.Session.AccountId; // Represents your {ACCOUNT_ID}
-
-            // Call the Examples API method to create an envelope and 
-            // add recipient that is to be authenticated with phone call
-            string envelopeId = RecipientAuthPhone.CreateEnvelopeWithRecipientUsingPhoneAuth(signerEmail, signerName,
-                accessToken, basePath, accountId, signerCountryCode, signerPhoneNumber);
-
-            // Process results
-            ViewBag.h1 = "Envelope sent";
-            ViewBag.message = "The envelope has been created and sent!<br />Envelope ID " + envelopeId + ".";
-            return View("example_done");
+                return View("Error");
+            }
         }
     }
 }
