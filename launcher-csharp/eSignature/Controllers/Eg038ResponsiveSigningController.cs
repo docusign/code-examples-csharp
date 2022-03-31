b6cc765fd6dec5a1fc5f5f2716134921452ee8ca
annahileta <anna.hileta@sigma.software>
2022-03-31 19:01:27 +0300

    added a responsive signing code example

---
+++ launcher-csharp/eSignature/Controllers/Eg038ResponsiveSigningController.cs
@@ -0,0 +1,46 @@
+﻿using DocuSign.CodeExamples.Controllers;
+using DocuSign.CodeExamples.Models;
+using Microsoft.AspNetCore.Mvc;
+using eSignature.Examples;
+
+namespace DocuSign.CodeExamples.Views
+{
+    [Area("eSignature")]
+    [Route("Eg038")]
+    public class Eg038ResponsiveSigningController : EgController
+    {
+        private string dsPingUrl;
+        private string signerClientId = "1000";
+        private string dsReturnUrl;
+
+        public Eg038ResponsiveSigningController(DSConfiguration config, IRequestItemsService requestItemsService)
+            : base(config, requestItemsService)
+        {            
+            dsPingUrl = config.AppUrl + "/";
+            dsReturnUrl = config.AppUrl + "/dsReturn";           
+            ViewBag.title = "Embedded Signing Ceremony";
+        }
+
+        public override string EgName => "Eg038";
+
+        [HttpPost]
+        public IActionResult Create(string signerEmail, string signerName, string ccEmail, string ccName)
+        {
+            string accessToken = RequestItemsService.User.AccessToken;
+            string basePath = RequestItemsService.Session.BasePath + "/restapi";
+            string accountId = RequestItemsService.Session.AccountId;
+
+            bool tokenOk = CheckToken(3);
+            if (!tokenOk)
+            {
+                RequestItemsService.EgName = EgName;
+                return Redirect("/ds/mustAuthenticate");
+            }
+
+            string redirectUrl = ResponsiveSigning.CreateEnvelopeFromHTML(signerEmail,
+                signerName, ccEmail, ccName, signerClientId, accessToken, basePath, accountId, dsReturnUrl, dsPingUrl);
+
+            return Redirect(redirectUrl);
+        }
+    }
+}
---
+++ launcher-csharp/eSignature/Examples/ResponsiveSigning.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DocuSign.eSign.Api;
+using DocuSign.eSign.Client;
+using DocuSign.eSign.Model;
+
+namespace eSignature.Examples
+{
+    public static class ResponsiveSigning
+    {
+        public static string CreateEnvelopeFromHTML(
+            string signerEmail, 
+            string signerName, 
+            string ccEmail, 
+            string ccName, 
+            string signerClientId, 
+            string accessToken, 
+            string basePath, 
+            string accountId, 
+            string returnUrl, 
+            string pingUrl = null)
+        {
+            EnvelopeDefinition envelope = MakeEnvelope(signerEmail, signerName, ccEmail, ccName, signerClientId);
+
+            ApiClient apiClient = new ApiClient(basePath);
+            apiClient.Configuration.DefaultHeader.Add("Authorization", "Bearer " + accessToken);
+
+            EnvelopesApi envelopesApi = new EnvelopesApi(apiClient);
+            EnvelopeSummary envelopeSummary = envelopesApi.CreateEnvelope(accountId, envelope);
+            string envelopeId = envelopeSummary.EnvelopeId;
+
+            RecipientViewRequest viewRequest = MakeRecipientViewRequest(signerEmail, signerName, returnUrl, signerClientId, pingUrl);
+
+            ViewUrl viewUrl = envelopesApi.CreateRecipientView(accountId, envelopeId, viewRequest);
+
+            return viewUrl.Url;
+        }
+
+        private static RecipientViewRequest MakeRecipientViewRequest(
+            string signerEmail, 
+            string signerName, 
+            string returnUrl, 
+            string signerClientId, 
+            string pingUrl = null)
+        {
+            RecipientViewRequest viewRequest = new RecipientViewRequest();
+            viewRequest.ReturnUrl = returnUrl + "?state=123";
+            viewRequest.AuthenticationMethod = "none";
+
+            viewRequest.Email = signerEmail;
+            viewRequest.UserName = signerName;
+            viewRequest.ClientUserId = signerClientId;
+
+            if (pingUrl != null)
+            {
+                viewRequest.PingFrequency = "600";
+                viewRequest.PingUrl = pingUrl;
+            }
+
+            return viewRequest;
+        }
+
+        private static EnvelopeDefinition MakeEnvelope(
+            string signerEmail, 
+            string signerName, 
+            string ccEmail, 
+            string ccName, 
+            string signerClientId)
+        {
+            SignHere signHere = new SignHere
+            {
+                StampType = "signature",
+                Name = "SignHere",
+                TabLabel = "signatureTab",
+                ScaleValue = "1",
+                Optional = "false",
+                DocumentId = "1",
+                RecipientId = "1",
+                PageNumber = "1",
+                XPosition = "143",
+                YPosition = "440"
+            };
+
+            Signer signer = new Signer
+            {
+                Email = signerEmail,
+                Name = signerName,
+                ClientUserId = signerClientId,
+                RecipientId = "1",
+                RoutingOrder = "1",
+                Tabs = new Tabs
+                {
+                    SignHereTabs = new List<SignHere> { signHere}
+                }
+            };
+
+            CarbonCopy cc = new CarbonCopy
+            {
+                Email = ccEmail,
+                Name = ccName,
+                RecipientId = "2",
+                RoutingOrder = "1"
+            };
+
+            string htmlMarkup = System.IO.File.ReadAllText("order_form.html");
+
+            string htmlWithData = htmlMarkup.Replace("{signerName}", signerName)
+                    .Replace("{signerEmail}", signerEmail)
+                    .Replace("{ccName}", ccName)
+                    .Replace("{ccEmail}", ccEmail);
+
+            return new EnvelopeDefinition()
+            {
+                EmailSubject = "Example Signing Document",
+                Documents = new List<Document>
+                {
+                    new Document
+                    {
+                        Name = "Lorem Ipsum",
+                        DocumentId = "1",
+                        HtmlDefinition = new DocumentHtmlDefinition
+                        {
+                            Source = htmlWithData
+                        }
+                    }
+                },
+                Recipients = new Recipients
+                {
+                    Signers = new List<Signer> { signer },
+                    CarbonCopies = new List<CarbonCopy> { cc }
+                },
+                Status = "sent"
+            };
+        }
+    }
+}
