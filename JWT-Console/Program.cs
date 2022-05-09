e6231dbe0f28613b0624691c03d8979d3a920943
Patrick Mackle <patrick.mackle@docusign.com>
2022-05-09 12:13:17 -0700

    fixed URL issue

--- JWT-Console/Program.cs
+++ JWT-Console/Program.cs
@@ -8,6 +8,8 @@ using System.Diagnostics;
 using System.Configuration;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Web;
+using System.IO;
 
 namespace DocuSign.CodeExamples.JWT_Console
 {
@@ -29,28 +31,32 @@ namespace DocuSign.CodeExamples.JWT_Console
                 // Consent for impersonation must be obtained to use JWT Grant
                 if (apiExp.Message.Contains("consent_required"))
                 {
-                    string url;
+                    // Caret needed for escaping & in windows URL
+                    string caret = "";
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        caret = "^";
+                    }
+
+                    // build a URL to provide consent for this Integration Key and this userId
+                    string url = "https://" + ConfigurationManager.AppSettings["AuthServer"] + "/oauth/auth?response_type=code" + caret + "&scope=impersonation%20signature" + caret +
+                        "&client_id=" + ConfigurationManager.AppSettings["ClientId"] + caret + "&redirect_uri=" + DevCenterPage;
+                    Console.WriteLine($"Consent is required - launching browser (URL is {url})");
+
                     // Start new browser window for login and consent to this app by DocuSign user
                     if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                     {
-                        // build a URL to provide consent for this Integratio Key and this userId
-                        url = "https://" + ConfigurationManager.AppSettings["AuthServer"] + "/oauth/auth?response_type=code^&scope=impersonation%20signature^&client_id=" +
-                                    ConfigurationManager.AppSettings["ClientId"] + "^&redirect_uri=" + DevCenterPage;
                         Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = false });
                     }
                     else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
                     {
-                        url = "https://" + ConfigurationManager.AppSettings["AuthServer"] + "/oauth/auth?response_type=code&scope=impersonation%20signature&client_id=" +
-                                    ConfigurationManager.AppSettings["ClientId"] + "&redirect_uri=" + DevCenterPage;
                         Process.Start("xdg-open", url);
                     }
-                    else
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                     {
-                        url = "https://" + ConfigurationManager.AppSettings["AuthServer"] + "/oauth/auth?response_type=code&scope=impersonation%20signature&client_id=" +
-                                    ConfigurationManager.AppSettings["ClientId"] + "&redirect_uri=" + DevCenterPage;
                         Process.Start("open", url);
                     }
-                    Console.WriteLine($"Consent is required - launched browser (URL is {url})");
+
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Unable to send envelope; Exiting. Please rerun the console app once consent was provided");
                     Console.ForegroundColor = ConsoleColor.White;
@@ -72,8 +78,8 @@ namespace DocuSign.CodeExamples.JWT_Console
             string ccEmail = Console.ReadLine();
             Console.Write("Enter the carbon copy's name: ");
             string ccName = Console.ReadLine();
-            string docDocx = @"../../../../launcher-csharp/World_Wide_Corp_salary.docx";
-            string docPdf = @"../../../../launcher-csharp/World_Wide_Corp_lorem.pdf";
+            string docDocx = Path.Combine(@"..", "..", "..", "..", "launcher-csharp", "World_Wide_Corp_salary.docx");
+            string docPdf = Path.Combine(@"..", "..", "..", "..", "launcher-csharp", "World_Wide_Corp_lorem.pdf");
             Console.WriteLine("");
             string envelopeId = SigningViaEmail.SendEnvelopeViaEmail(signerEmail, signerName, ccEmail, ccName, accessToken.access_token, acct.BaseUri + "/restapi", acct.AccountId, docDocx, docPdf, "sent");
             Console.WriteLine("");
