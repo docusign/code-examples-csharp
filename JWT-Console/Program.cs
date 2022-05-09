446901208495b89c24a3c4882a43e43b3ec0b6e4
Patrick Mackle <pimackle@ucdavis.edu>
2022-05-09 15:17:57 -0700

    Fix MacOS JWT bug

--- JWT-Console/Program.cs
+++ JWT-Console/Program.cs
@@ -7,6 +7,9 @@ using System;
 using System.Diagnostics;
 using System.Configuration;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.IO;
 
 namespace DocuSign.CodeExamples.JWT_Console
 {
@@ -28,12 +31,31 @@ namespace DocuSign.CodeExamples.JWT_Console
                 // Consent for impersonation must be obtained to use JWT Grant
                 if (apiExp.Message.Contains("consent_required"))
                 {
+                    string caret = "";
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        caret = "^";
+                    }
+
                     // build a URL to provide consent for this Integratio Key and this userId
-                    string url = "https://" + ConfigurationManager.AppSettings["AuthServer"] + "/oauth/auth?response_type=code^&scope=impersonation%20signature^&client_id=" +
-                                ConfigurationManager.AppSettings["ClientId"] + "^&redirect_uri=" + DevCenterPage;
+                    string url = "https://" + ConfigurationManager.AppSettings["AuthServer"] + "/oauth/auth?response_type=code" + caret + "&scope=impersonation%20signature"
+                        + caret + "&client_id=" + ConfigurationManager.AppSettings["ClientId"] + caret + "&redirect_uri=" + DevCenterPage;
                     Console.WriteLine($"Consent is required - launching browser (URL is {url})");
+
                     // Start new browser window for login and consent to this app by DocuSign user
-                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = false });
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = false });
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    {
+                        Process.Start("xdg-open", url);
+                    }
+                    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    {
+                        Process.Start("open", url);
+                    }
+
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Unable to send envelope; Exiting. Please rerun the console app once consent was provided");
                     Console.ForegroundColor = ConsoleColor.White;
@@ -55,8 +77,8 @@ namespace DocuSign.CodeExamples.JWT_Console
             string ccEmail = Console.ReadLine();
             Console.Write("Enter the carbon copy's name: ");
             string ccName = Console.ReadLine();
-            string docDocx = @"..\..\..\..\launcher-csharp\World_Wide_Corp_salary.docx";
-            string docPdf = @"..\..\..\..\launcher-csharp\World_Wide_Corp_lorem.pdf";
+            string docDocx = Path.Combine(@"..", "..", "..", "..", "launcher-csharp", "World_Wide_Corp_salary.docx");
+            string docPdf = Path.Combine(@"..", "..", "..", "..", "launcher-csharp", "World_Wide_Corp_lorem.pdf");
             Console.WriteLine("");
             string envelopeId = SigningViaEmail.SendEnvelopeViaEmail(signerEmail, signerName, ccEmail, ccName, accessToken.access_token, acct.BaseUri + "/restapi", acct.AccountId, docDocx, docPdf, "sent");
             Console.WriteLine("");
