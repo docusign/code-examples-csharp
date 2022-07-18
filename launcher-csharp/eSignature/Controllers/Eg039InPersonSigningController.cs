0244ff66f509431b469b78c184164a83616900bd
annahileta <anna.hileta@sigma.software>
2022-07-18 12:54:11 +0300

    added the in-person signing code example

---
+++ launcher-csharp/eSignature/Controllers/Eg039InPersonSigningController.cs
@@ -0,0 +1,57 @@
+﻿using DocuSign.CodeExamples.Controllers;
+using DocuSign.CodeExamples.Models;
+using Microsoft.AspNetCore.Mvc;
+using eSignature.Examples;
+using DocuSign.eSign.Client;
+
+namespace DocuSign.CodeExamples.Views
+{
+    [Area("eSignature")]
+    [Route("Eg039")]
+    public class Eg039InPersonSigningController : EgController
+    {
+        private string dsPingUrl;
+        private string dsReturnUrl;
+
+        public Eg039InPersonSigningController(DSConfiguration config, IRequestItemsService requestItemsService)
+            : base(config, requestItemsService)
+        {
+            dsPingUrl = config.AppUrl + "/";
+            dsReturnUrl = config.AppUrl + "/dsReturn";           
+            ViewBag.title = "In Person Signing";
+        }
+
+        public override string EgName => "Eg039";
+
+        [HttpPost]
+        public IActionResult Create(string hostEmail, string hostName, string signerName)
+        {
+            string accessToken = RequestItemsService.User.AccessToken;
+            string basePath = RequestItemsService.Session.BasePath + "/restapi";
+            string accountId = RequestItemsService.Session.AccountId;
+            string redirectUrl = string.Empty;
+
+            bool tokenOk = CheckToken(3);
+            if (!tokenOk)
+            {
+                RequestItemsService.EgName = EgName;
+                return Redirect("/ds/mustAuthenticate");
+            }
+
+            try
+            {
+                redirectUrl = InPersonSigning.SendEnvelopeForInPersonSigning(hostEmail, hostName, signerName, accessToken, 
+                    basePath, accountId, Config.docPdf, dsReturnUrl, dsPingUrl);
+            } 
+            catch (ApiException apiException) 
+            {
+                ViewBag.errorCode = apiException.ErrorCode;
+                ViewBag.errorMessage = apiException.Message;
+
+                return View("Error");
+            }
+
+            return Redirect(redirectUrl);
+        }
+    }
+}
---
+++ launcher-csharp/eSignature/Examples/InPersonSigning.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using DocuSign.eSign.Api;
+using DocuSign.eSign.Client;
+using DocuSign.eSign.Model;
+
+namespace eSignature.Examples
+{
+    public static class InPersonSigning
+    {
+        /// <summary>
+        /// Creates a new envelope, adds a single document and an in-person signer and generates a url that is used for embedded signing.
+        /// </summary>
+        /// <param name="hostEmail">Email address for the host</param>
+        /// <param name="hostName">Full name of the host</param>
+        /// <param name="signerEmail">Email address for the signer</param>
+        /// <param name="accessToken">Access Token for API call (OAuth)</param>
+        /// <param name="basePath">BasePath for API calls (URI)</param>
+        /// <param name="accountId">The DocuSign Account ID (GUID or short version) for which the APIs call would be made</param>
+        /// <param name="docPdf">String of bytes representing the document (pdf)</param>
+        /// <param name="returnUrl">URL user will be redirected to after they sign</param>
+        /// <param name="pingUrl">URL that DocuSign will be able to ping to incdicate signing session is active</param>
+        /// <returns>The URL for the embedded signing</returns>
+        public static string SendEnvelopeForInPersonSigning(string hostEmail, string hostName, string signerEmail, string accessToken, 
+            string basePath, string accountId, string docPdf, string returnUrl, string pingUrl = null)
+        {
+            EnvelopeDefinition envelopeDefinition = PrepareEnvelope(hostEmail, hostName, signerEmail, docPdf);
+                  
+            var apiClient = new ApiClient(basePath);
+            apiClient.Configuration.DefaultHeader.Add("Authorization", "Bearer " + accessToken);
+
+            EnvelopesApi envelopesApi = new EnvelopesApi(apiClient);
+
+            EnvelopeSummary envelopeSummary = envelopesApi.CreateEnvelope(accountId, envelopeDefinition);
+            RecipientViewRequest viewRequest = MakeRecipientViewRequest(hostEmail, hostName, returnUrl, pingUrl);
+            ViewUrl viewUrl = envelopesApi.CreateRecipientView(accountId, envelopeSummary.EnvelopeId, viewRequest);
+            
+            return viewUrl.Url;
+        }
+
+        private static RecipientViewRequest MakeRecipientViewRequest(
+            string hostEmail, 
+            string hostName, 
+            string returnUrl, 
+            string pingUrl = null)
+        {
+            var recipientViewRequest = new RecipientViewRequest
+            {
+                ReturnUrl = returnUrl + "?state=123",
+                AuthenticationMethod = "none",
+                Email = hostEmail,
+                UserName = hostName
+            };
+
+            if (pingUrl != null)
+            {
+                recipientViewRequest.PingFrequency = "600"; 
+                recipientViewRequest.PingUrl = pingUrl;
+            }
+            return recipientViewRequest;
+        }
+
+        private static EnvelopeDefinition PrepareEnvelope(string hostEmail, string hostName, string signerName, string docPdf)
+        {
+            byte[] fileContentInBytes = System.IO.File.ReadAllBytes(docPdf);
+
+            EnvelopeDefinition envelopeDefinition = new EnvelopeDefinition
+            {
+                EmailSubject = "Please host this in-person signing session",
+                Documents = new List<Document> {
+                    new Document
+                    {
+                        DocumentBase64 = Convert.ToBase64String(fileContentInBytes),
+                        Name = "Lorem Ipsum",
+                        FileExtension = "pdf",
+                        DocumentId = "1"
+                    }
+                }
+            };
+
+            InPersonSigner inPersonSigner = new InPersonSigner
+            {
+                HostEmail = hostEmail,
+                HostName = hostName,
+                SignerName = signerName,
+                RecipientId = "1",
+                RoutingOrder = "1",
+                Tabs = new Tabs
+                {
+                    SignHereTabs = new List<SignHere>
+                    {
+                        new SignHere
+                        {
+                            AnchorString = "/sn1/",
+                            AnchorUnits = "pixels",
+                            AnchorXOffset = "20",
+                            AnchorYOffset = "10"
+                        }
+                    }
+                }
+            };
+
+            envelopeDefinition.Recipients = new Recipients
+            {
+                InPersonSigners = new List<InPersonSigner>
+                {
+                    inPersonSigner
+                }
+            };
+
+            envelopeDefinition.Status = "sent";
+
+            return envelopeDefinition;
+        }
+    }
+}
