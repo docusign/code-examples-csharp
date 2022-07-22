f9a309f7fc686384a372b882138e4b51ecc77cbf
Inbar Gazit <inbar.gazit@docusign.com>
2022-07-22 15:08:13 -0700

    Adding new In Person Signer code example

---
+++ launcher-csharp/eSignature/Controllers/Eg039InPersonSigningController.cs
@@ -0,0 +1,59 @@
+﻿using DocuSign.CodeExamples.Controllers;
+using DocuSign.CodeExamples.Models;
+using Microsoft.AspNetCore.Mvc;
+using eSignature.Examples;
+using DocuSign.eSign.Client;
+
+namespace DocuSign.CodeExamples.Views
+{
+    [Area("eSignature")]
+    [Route("Eg039")]
+    public class Eg039InPersonSigningController : EgController
+    {
+        private string DsPingUrl { get; set; }
+        private string DsReturnUrl { get; set; }
+
+        public Eg039InPersonSigningController(DSConfiguration config, IRequestItemsService requestItemsService)
+            : base(config, requestItemsService)
+        {
+            DsPingUrl = config.AppUrl + "/";
+            DsReturnUrl = config.AppUrl + "/dsReturn";           
+            ViewBag.title = "In Person Signing";
+        }
+
+        public override string EgName => "Eg039";
+
+        [HttpPost]
+        public IActionResult Create(string signerName)
+        {
+            string accessToken = RequestItemsService.User.AccessToken;
+            string basePath = RequestItemsService.Session.BasePath + "/restapi";
+            string accountId = RequestItemsService.Session.AccountId;
+            string hostEmail = RequestItemsService.AuthenticatedUserEmail;
+            string hostName = RequestItemsService.User.Name;
+            string redirectUrl = string.Empty;
+
+            bool tokenOk = CheckToken(3);
+            if (!tokenOk)
+            {
+                RequestItemsService.EgName = EgName;
+                return Redirect("/ds/mustAuthenticate");
+            }
+
+            try
+            {
+                redirectUrl = InPersonSigning.SendEnvelopeForInPersonSigning(hostEmail, hostName, signerName, accessToken, 
+                    basePath, accountId, Config.docPdf, DsReturnUrl, DsPingUrl);
+            } 
+            catch (ApiException apiException) 
+            {
+                ViewBag.errorCode = apiException.ErrorCode;
+                ViewBag.errorMessage = apiException.Message;
+
+                return View("Error");
+            }
+
+            return Redirect(redirectUrl);
+        }
+    }
+}
--- launcher-csharp/wwwroot/js/site.js
+++ launcher-csharp/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $("#Products").on("change", function () {
+        $list = $("#PermissionProfilesFiltered");
+        $.ajax({
+            url: "/getPermissionProfiles",
+            type: "GET",
+            data: { productId: $("#Products").val() },
+            traditional: true,
+            success: function (result) {
+                $list.empty();
+                $.each(result, function (i, item) {
+                    $list.append('<option value="' + item["permissionProfileId"] + '"> ' + item["permissionProfileName"] + ' </option>');
+                });
+            },
+            error: function () {
+                console.warn("Issues with calling the getPermissionProfiles endpoint. Please, check the solution.");
+            }
+        });
+    });
+});
