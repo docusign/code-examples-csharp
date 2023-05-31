f67140164f5e7bb51683003e9927e3e1844b0d4a
Inbar Gazit <inbar.gazit@docusign.com>
2023-05-31 12:59:21 -0700

    Various updates including two new code examples for Admin API

---
+++ launcher-csharp/Admin/Controllers/DeleteUserDataFromOrganization.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="DeleteUserDataFromOrganization.cs" company="DocuSign">
+// Copyright (c) DocuSign. All rights reserved.
+// </copyright>
+
+namespace DocuSign.CodeExamples.Admin.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DocuSign.Admin.Client;
+    using DocuSign.Admin.Examples;
+    using DocuSign.Admin.Model;
+    using DocuSign.CodeExamples.Common;
+    using DocuSign.CodeExamples.Controllers;
+    using DocuSign.CodeExamples.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+
+    [Area("Admin")]
+    [Route("Aeg010")]
+    public class DeleteUserDataFromOrganization : EgController
+    {
+        public DeleteUserDataFromOrganization(DSConfiguration config, LauncherTexts launcherTexts, IRequestItemsService requestItemsService)
+            : base(config, launcherTexts, requestItemsService)
+        {
+            this.CodeExampleText = this.GetExampleText(EgName, ExamplesAPIType.Admin);
+            this.ViewBag.title = this.CodeExampleText.ExampleName;
+        }
+
+        public override string EgName => "Aeg010";
+
+        [MustAuthenticate]
+        [SetViewBag]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteUserDataFromOrganizationByEmail(string email)
+        {
+            try
+            {
+                Guid? organizationId = this.RequestItemsService.OrganizationId;
+                string accessToken = this.RequestItemsService.User.AccessToken;
+                string basePath = this.RequestItemsService.Session.AdminApiBasePath;
+
+                var response = DocuSign.Admin.Examples.DeleteUserDataFromOrganization.DeleteUserDataFromOrganizationByEmail(
+                    basePath,
+                    accessToken,
+                    organizationId.Value,
+                    email);
+
+                this.ViewBag.h1 = this.CodeExampleText.ExampleName;
+                this.ViewBag.message = this.CodeExampleText.ResultsPageText;
+                this.ViewBag.Locals.Json = JsonConvert.SerializeObject(response, Formatting.Indented);
+
+                return this.View("example_done");
+            }
+            catch (ApiException apiException)
+            {
+                this.ViewBag.errorCode = apiException.ErrorCode;
+                this.ViewBag.errorMessage = apiException.Message;
+                this.ViewBag.SupportingTexts = this.LauncherTexts.ManifestStructure.SupportingTexts;
+
+                return this.View("Error");
+            }
+        }
+    }
+}
