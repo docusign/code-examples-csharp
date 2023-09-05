9a2a90079cb3fdb04f7f02018172bda05564811e
annahileta <anna.hileta@sigma.software>
2023-09-05 16:00:30 +0300

    added clone account code example

---
+++ launcher-csharp/Admin/Examples/CloneAccount.cs
@@ -0,0 +1,87 @@
+﻿// <copyright file="CloneAccount.cs" company="DocuSign">
+// Copyright (c) DocuSign. All rights reserved.
+// </copyright>
+
+namespace DocuSign.Admin.Examples
+{
+    using System;
+    using DocuSign.Admin.Api;
+    using DocuSign.Admin.Client;
+    using DocuSign.Admin.Model;
+
+    public class CloneAccount
+    {
+        private static readonly string AuthorizationHeader = "Authorization";
+        private static readonly string Bearer = "Bearer ";
+
+        /// <summary>
+        /// Get all accounts in asset groups for the organization. Required scopes: asset_group_account_read
+        /// </summary>
+        /// <param name="basePath">BasePath for API calls (URI)</param>
+        /// <param name="accessToken">Access Token for API call (OAuth)</param>
+        /// <param name="orgId">DocuSign Organization Id (GUID)</param>
+        /// <returns>AssetGroupAccountsResponse</returns>
+        public static AssetGroupAccountsResponse GetGroupAccounts(string basePath, string accessToken, Guid? orgId)
+        {
+            DocuSignClient docuSignClient = new DocuSignClient(basePath);
+            docuSignClient.Configuration.DefaultHeader.Add(AuthorizationHeader, Bearer + accessToken);
+
+            ProvisionAssetGroupApi assetGroupApi = new ProvisionAssetGroupApi(docuSignClient);
+            ProvisionAssetGroupApi.GetAssetGroupAccountsOptions options = new ProvisionAssetGroupApi.GetAssetGroupAccountsOptions
+            {
+                compliant = true,
+            };
+
+            return assetGroupApi.GetAssetGroupAccounts(orgId, options);
+        }
+
+        /// <summary>
+        /// Clones an existing DocuSign account to a new DocuSign account.
+        /// </summary>
+        /// <param name="basePath">BasePath for API calls (URI)</param>
+        /// <param name="accessToken">Access Token for API call (OAuth)</param>
+        /// <param name="orgId">DocuSign Organization Id (GUID)</param>
+        /// <param name="sourceAccountId">Source account Id (GUID)</param>
+        /// <param name="targetAccountName">Target account name (string)</param>
+        /// <param name="targetAccountFirstName">Target account first name (string)</param>
+        /// <param name="targetAccountLastName">Target account last name (string)</param>
+        /// <param name="targetAccountEmail">Target account email (string)</param>
+        /// <returns>AssetGroupAccountClone</returns>
+        public static AssetGroupAccountClone CloneGroupAccount(
+            string basePath,
+            string accessToken,
+            Guid? orgId,
+            Guid? sourceAccountId,
+            string targetAccountName,
+            string targetAccountFirstName,
+            string targetAccountLastName,
+            string targetAccountEmail)
+        {
+            var docuSignClient = new DocuSignClient(basePath);
+            docuSignClient.Configuration.DefaultHeader.Add(AuthorizationHeader, Bearer + accessToken);
+
+            string countryCode = "US";
+            var accountData = new AssetGroupAccountClone
+            {
+                SourceAccount = new AssetGroupAccountCloneSourceAccount
+                {
+                    Id = sourceAccountId,
+                },
+                TargetAccount = new AssetGroupAccountCloneTargetAccount
+                {
+                    Name = targetAccountName,
+                    Admin = new AssetGroupAccountCloneTargetAccountAdmin
+                    {
+                        FirstName = targetAccountFirstName,
+                        LastName = targetAccountLastName,
+                        Email = targetAccountEmail,
+                    },
+                    CountryCode = countryCode,
+                },
+            };
+
+            var assetGroupApi = new ProvisionAssetGroupApi(docuSignClient);
+            return assetGroupApi.CloneAssetGroupAccount(orgId, accountData);
+        }
+    }
+}
