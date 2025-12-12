6e3dcb234b0dd739dec986eaf690b6aa80ae5980
anna <anna.hileta@sigma.software>
2025-12-12 18:02:36 +0100

    Moved DateTime in CreateBrand.cs

--- launcher-csharp/eSignature/Examples/CreateBrand.cs
+++ launcher-csharp/eSignature/Examples/CreateBrand.cs
@@ -46,10 +46,9 @@ namespace ESignature.Examples
             response.Headers.TryGetValue("X-RateLimit-Remaining", out string remaining);
             response.Headers.TryGetValue("X-RateLimit-Reset", out string reset);
 
-            DateTime resetDate = DateTimeOffset.FromUnixTimeSeconds(long.Parse(reset)).UtcDateTime;
-
             if (reset != null && remaining != null)
             {
+                DateTime resetDate = DateTimeOffset.FromUnixTimeSeconds(long.Parse(reset)).UtcDateTime;
                 Console.WriteLine("API calls remaining: " + remaining);
                 Console.WriteLine("Next Reset: " + resetDate);
             }
